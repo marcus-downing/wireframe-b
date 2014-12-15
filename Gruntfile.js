0f081b5c603d370f1928fb97e44fe7ecfaf7ecc4
Marcus Downing <marcus@bang-on.net>
2014-12-15 15:46:11 +0000

    Remove gruntfile: this cannot be built standalone

--- Gruntfile.js
+++
@@ -1,8 +0,0 @@
-module.exports = function (grunt) {
-  var path = require("path");
-  grunt.sources = [
-    path.resolve(".")
-  ];
-  grunt.dest = path.resolve("..");
-  require("./grunt/config.js")(grunt);
-};
