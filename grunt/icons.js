9f490d46a3239e8553e1bcf0ab47e19e548e037d
Marcus Downing <marcus@bang-on.net>
2014-12-19 15:26:33 +0000

    Icons processing

---
+++ grunt/icons.js
@@ -0,0 +1,19 @@
+module.exports = function (grunt, _) {
+  grunt.loadNpmTasks('grunt-webfont');
+
+  var icon_files = grunt.locateFiles("icons", "*.svg");
+  if (grunt.debug) console.log("Icons: "+JSON.stringify(icon_files, null, 4));
+
+  grunt.config.merge({
+    webfont: {
+      icons: {
+        src: icon_files,
+        dest: grunt.dest+'/fonts',
+        options: {
+          syntax: 'bootstrap',
+          autoHint: false,
+        }
+      }
+    },
+  });
+};
