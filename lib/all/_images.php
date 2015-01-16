8c9f394653d4261be9e44d5fb691d39cdc10002e
Marcus Downing <marcus@bang-on.net>
2015-01-16 13:04:32 +0000

    Responsive images for all

--- lib/all/_images.php
+++ lib/all/_images.php
@@ -2,7 +2,17 @@
 
 namespace wireframe_b\admin;
 
-function the_picture($file, $echo = true) {
+/*
+  Function to output <picture> and srcset= correctly
+*/
+
+function the_picture($file, $args) {
+  $args = wp_parse_args($args, array(
+    'alt' => null,
+    'title' => null,
+    'echo' => true
+    ));
+
   $url = get_stylesheet_directory_uri().'/images/'.$file;
   $file = get_template_directory().'/images/'.$file;
 
@@ -13,13 +23,16 @@ function the_picture($file, $echo = true) {
   foreach (array("2x", "4x") as $x) {
     $bigurl = $pathinfo['dirname'].'/'.$pathinfo['filename'].'@'.$x.'.'.$pathinfo['extension'];
     $bigfilename = $file_pathinfo['dirname'].'/'.$pathinfo['filename'].'@'.$x.'.'.$pathinfo['extension'];
-    
+
     if (file_exists($bigfilename))
       $srcset[] = "$bigurl $x";
   }
   $srcset = empty($srcset) ? '' : " srcset='".esc_attr(implode(', ', $srcset))."'";
-  $img = "<img src='".esc_attr($url)."' $srcset/>";
 
-  if ($echo) echo $img;
+  $alt = is_null($args['alt']) ? '' : " alt='".esc_attr($args['alt'])."'";
+  $title = is_null($args['title']) ? '' : " title='".esc_attr($args['title'])."'";
+  $img = "<img src='".esc_attr($url)."'$srcset$alt$title/>";
+
+  if ($args['echo']) echo $img;
   else return $img;
 }
