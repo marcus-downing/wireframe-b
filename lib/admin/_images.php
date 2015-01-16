4c25c4dabd542d018dc5761fb425bb13b9f2e4fe
Marcus Downing <marcus@bang-on.net>
2015-01-16 12:54:24 +0000

    Fix the pathinfo for correct srcset

--- lib/admin/_images.php
+++ lib/admin/_images.php
@@ -3,22 +3,22 @@
 namespace wireframe_b\admin;
 
 function the_picture($file, $echo = true) {
-  $url = get_bloginfo('template_url').'/images/'.$file;
-  $file = get_bloginfo('template_directory').'/images/'.$file;
+  $url = get_stylesheet_directory_uri().'/images/'.$file;
+  $file = get_template_directory().'/images/'.$file;
 
   $pathinfo = pathinfo($url);
   $file_pathinfo = pathinfo($file);
 
   $srcset = array();
   foreach (array("2x", "4x") as $x) {
-    $bigurl = $pathinfo['dirname'].'/'.$pathinfo['basename'].'@'.$x.$pathinfo['extension'];
-    $bigfilename = $file_pathinfo['dirname'].'/'.$pathinfo['basename'].'@'.$x.$pathinfo['extension'];
-
-    do_action('log', 'Texting file', $bigfilename);
+    $bigurl = $pathinfo['dirname'].'/'.$pathinfo['filename'].'@'.$x.'.'.$pathinfo['extension'];
+    $bigfilename = $file_pathinfo['dirname'].'/'.$pathinfo['filename'].'@'.$x.'.'.$pathinfo['extension'];
+    
     if (file_exists($bigfilename))
       $srcset[] = "$bigurl $x";
   }
-  $img = "<img src='".esc_attr($url)."' srcset='".esc_attr(implode(', ', $srcset))."' />";
+  $srcset = empty($srcset) ? '' : " srcset='".esc_attr(implode(', ', $srcset))."'";
+  $img = "<img src='".esc_attr($url)."' $srcset/>";
 
   if ($echo) echo $img;
   else return $img;
